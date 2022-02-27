e4b52856e2f6b8f5fb17c54c2b3f36808f0694d9
Afolabi Opakunle <opakunleafolabi@gmail.com>
2022-02-27 21:33:01 +0100

    sign out

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,15 +1,19 @@
 import { Component, OnInit } from '@angular/core';
+import { AngularFireAuth } from "@angular/fire/compat/auth";
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.css']
 })
-export class NavbarComponent implements OnInit {
+export class NavbarComponent {
 
-  constructor() { }
+  constructor(private afAuth: AngularFireAuth) {
+    afAuth.authState.subscribe(response =>  console.log(response))
+  }
 
-  ngOnInit(): void {
+  signOut() {
+    this.afAuth.signOut();
   }
 
 }
