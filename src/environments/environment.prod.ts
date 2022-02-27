0b8f509ecaeb4d49617fd5f6638576e31443fda8
Afolabi Opakunle <opakunleafolabi@gmail.com>
2022-02-27 17:23:47 +0100

    bootstrap added

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    projectId: 'organic-oshop-5eed4',
+    appId: '1:882442420617:web:6493d64f131ea9a0a22158',
+    storageBucket: 'organic-oshop-5eed4.appspot.com',
+    apiKey: 'AIzaSyAaK3xvWMS2YnSND_svOFC6nF13K5MmlJY',
+    authDomain: 'organic-oshop-5eed4.firebaseapp.com',
+    messagingSenderId: '882442420617',
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    projectId: 'organic-oshop-5eed4',
+    appId: '1:882442420617:web:6493d64f131ea9a0a22158',
+    storageBucket: 'organic-oshop-5eed4.appspot.com',
+    apiKey: 'AIzaSyAaK3xvWMS2YnSND_svOFC6nF13K5MmlJY',
+    authDomain: 'organic-oshop-5eed4.firebaseapp.com',
+    messagingSenderId: '882442420617',
+  }
 };
 
 /*
